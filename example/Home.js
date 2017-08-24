73c90678173705e262c19788d9038a8179e7d23b
吕杰 <991102@nd.com>
2017-08-24 17:53:46 +0800

    初步完成android的使用

--- example/Home.js
+++ example/Home.js
@@ -48,17 +48,22 @@ export default class Home extends React.Component{
                 />
 
 
-                <ModalDropdown
-                    style={styles.dropdown_1}
-                    textStyle={styles.dropdown_1_text}
-                    dropdownStyle={styles.dropdown_1_dropdown}
-                    dropdownTextStyle={styles.dropdown_1_dropdownTextStyle}
-                    options={DEMO_OPTIONS_1}
-                    defaultValue="按我"
-                    showsVerticalScrollIndicator = {false}
-                >
+                {/*<ModalDropdown*/}
+                    {/*style={styles.dropdown_1}*/}
+                    {/*textStyle={styles.dropdown_1_text}*/}
+                    {/*dropdownStyle={styles.dropdown_1_dropdown}*/}
+                    {/*dropdownTextStyle={styles.dropdown_1_dropdownTextStyle}*/}
+                    {/*options={DEMO_OPTIONS_1}*/}
+                    {/*defaultValue="按我"*/}
+                    {/*showsVerticalScrollIndicator = {false}*/}
+                {/*/>*/}
 
-                </ModalDropdown>
+                <ModalDropdown
+                    defaultValue='下拉列表'
+                    options={['公共微博','我的关注','微博热榜','我的收藏','99U消息']}
+                    onSelect={(index, value)=>{ToastAndroid.show('index='+index+'  value='+value, ToastAndroid.SHORT)}}
+                />
+
             </View>
         );
     }
@@ -75,19 +80,20 @@ const styles = StyleSheet.create({
         left: 8,
         width: 150,
     },
-    dropdown_1_text: {
+    dropdown_1_text: {      // 设置defaultValue文字的样式
         fontSize: 14,
         textAlign: 'center',
         textAlignVertical: 'center',
     },
-    dropdown_1_dropdown: {
+    dropdown_1_dropdown: {      // 设置下拉列表的样式
         width: 150,
         height: 200,
         borderColor: 'lightgray',
         borderWidth: StyleSheet.hairlineWidth,
         borderRadius: 4,
+        backgroundColor: 'lightgray',
     },
-    dropdown_1_dropdownTextStyle: {
+    dropdown_1_dropdownTextStyle: {     //  设置列表中文字的样式
         color: '#666666'
     },
 });
